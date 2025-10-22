129664c58050f85b4fdcb6ec89c160c927601907
avatarstudio3d <avatarstudio3d@gmail.com>
2025-10-23 04:19:38 +0800

    update

---
+++ js/loadtxt.js
@@ -0,0 +1,275 @@
+// SOAP
+$(function(){
+    $.get("./abs/soap.txt", function(data) {
+       $("#soapabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/soap.txt", function(data) {
+       $("#soapbib").html(data);
+    });
+ });
+ 
+ // Easi3R
+ $(function(){
+    $.get("./abs/easi3r.txt", function(data) {
+       $("#easi3rabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/easi3r.txt", function(data) {
+       $("#easi3rbib").html(data);
+    });
+ });
+ 
+ 
+ // ETCH
+ $(function(){
+    $.get("./abs/etch.txt", function(data) {
+       $("#etchabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/etch.txt", function(data) {
+       $("#etchbib").html(data);
+    });
+ });
+ 
+ // ChatGarment
+ $(function(){
+    $.get("./abs/chatgarment.txt", function(data) {
+       $("#chatgarmentabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/chatgarment.txt", function(data) {
+       $("#chatgarmentbib").html(data);
+    });
+ });
+ 
+ 
+ // Feat2GS
+ $(function(){
+    $.get("./abs/feat2gs.txt", function(data) {
+       $("#feat2gsabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/feat2gs.txt", function(data) {
+       $("#feat2gsbib").html(data);
+    });
+ });
+ 
+ 
+ // PuzzleAvatar
+ $(function(){
+    $.get("./abs/puzzleavatar.txt", function(data) {
+       $("#puzzleavatarabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/puzzleavatar.txt", function(data) {
+       $("#puzzleavatarbib").html(data);
+    });
+ });
+ 
+ // StableNormal
+ $(function(){
+    $.get("./abs/stablenormal.txt", function(data) {
+       $("#stablenormalabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/stablenormal.txt", function(data) {
+       $("#stablenormalbib").html(data);
+    });
+ });
+ 
+ // G-Shell
+ $(function(){
+    $.get("./abs/gshell.txt", function(data) {
+       $("#gshellabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/gshell.txt", function(data) {
+       $("#gshellbib").html(data);
+    });
+ });
+ 
+ // BOFT
+ $(function(){
+    $.get("./abs/boft.txt", function(data) {
+       $("#boftabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/boft.txt", function(data) {
+       $("#boftbib").html(data);
+    });
+ });
+ 
+ // TADA
+ $(function(){
+    $.get("./abs/tada.txt", function(data) {
+       $("#tadaabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/tada.txt", function(data) {
+       $("#tadabib").html(data);
+    });
+ });
+ 
+ // TeCH
+ $(function(){
+    $.get("./abs/tech.txt", function(data) {
+       $("#techabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/tech.txt", function(data) {
+       $("#techbib").html(data);
+    });
+ });
+ 
+ // DIF
+ $(function(){
+    $.get("./abs/dif.txt", function(data) {
+       $("#difabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/dif.txt", function(data) {
+       $("#difbib").html(data);
+    });
+ });
+ 
+ // CAR
+ $(function(){
+    $.get("./abs/car.txt", function(data) {
+       $("#carabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/car.txt", function(data) {
+       $("#carbib").html(data);
+    });
+ });
+ 
+ // ECON
+ $(function(){
+    $.get("./abs/econ.txt", function(data) {
+       $("#econabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/econ.txt", function(data) {
+       $("#econbib").html(data);
+    });
+ });
+ 
+ // ICON
+ $(function(){
+    $.get("./abs/icon.txt", function(data) {
+       $("#iconabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/icon.txt", function(data) {
+       $("#iconbib").html(data);
+    });
+ });
+ 
+ // Alphapose
+ $(function(){
+    $.get("./abs/alphapose.txt", function(data) {
+       $("#alphaposeabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/alphapose.txt", function(data) {
+       $("#alphaposebib").html(data);
+    });
+ });
+ 
+ // DART
+ $(function(){
+    $.get("./abs/dart.txt", function(data) {
+       $("#dartabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/dart.txt", function(data) {
+       $("#dartbib").html(data);
+    });
+ });
+ 
+ // PoseHD
+ $(function(){
+    $.get("./abs/posehd.txt", function(data) {
+       $("#posehdabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/posehd.txt", function(data) {
+       $("#posehdbib").html(data);
+    });
+ });
+ 
+ // MonoPort
+ $(function(){
+    $.get("./abs/monoport.txt", function(data) {
+       $("#monoportabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/monoport.txt", function(data) {
+       $("#monoportbib").html(data);
+    });
+ });
+ 
+ // MonoPort_RTL
+ $(function(){
+    $.get("./abs/monoport_rtl.txt", function(data) {
+       $("#monoportrtlabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/monoport_rtl.txt", function(data) {
+       $("#monoportrtlbib").html(data);
+    });
+ });
+ 
+ // PoseFlow
+ $(function(){
+    $.get("./abs/poseflow.txt", function(data) {
+       $("#poseflowabs").html(data);
+    });
+ });
+ 
+ $(function(){
+    $.get("./bib/poseflow.txt", function(data) {
+       $("#poseflowbib").html(data);
+    });
+ });
